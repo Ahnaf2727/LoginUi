96159429b2e5c5d4e004ef52c37e9bc1eaccc899
Ahnaf2727 <captain.ahnaf27@gmail.com>
2021-02-05 16:27:52 +0700

    clear

--- LoginUi/ContentView.swift
+++ LoginUi/ContentView.swift
@@ -9,8 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Image("assset login UI")
+            .resizable()
+            .edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                HandleView()
+                FormBox()
+            }
+        }
+    }
+}
+
+struct HandleView : View {
+    var body: some View{
+        VStack{
+            Image(systemName: "tv")
+            .resizable()
+            .frame(width: 100, height: 100)
+            .background(Color.black)
+            .foregroundColor(Color.orange)
             .padding()
+            .background(Color.black)
+            .cornerRadius(20)
+            
+            
+            Text("Hello User")
+            .foregroundColor(Color.white)
+            .bold()
+            .padding(5)
+        }
+    }
+}
+
+struct FormBox : View {
+    @State var username : String = ""
+    @State var password : String = ""
+    
+    var body: some View{
+        VStack (alignment: .leading){
+            Text ("Username").font(.callout).bold()
+            TextField("Username", text : $username )
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+            
+            Text ("Password").font(.callout).bold()
+            SecureField("Password", text: $password)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            
+            
+            Button(action: {print("Hello Button")}) { Text("Button")
+            }
+                //  design button
+            .padding()
+            .background(Color.gray)
+            .foregroundColor(Color.white)
+            .cornerRadius(10)
+            
+        }
+//        Design Vstack
+        .padding(.all, 20)
+        .background(Color.orange)
+        .cornerRadius(10)
+        
     }
 }
 
